c6690e5e36e2d2cb0275d3e40079c7b59986236b
vianeyUnamur <98978845+vianeyUnamur@users.noreply.github.com>
2022-08-06 18:51:39 +0200

    update code and add Dockerfile

--- backup.py
+++ backup.py
@@ -1,59 +1,72 @@
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
-import shutil, os, multiprocessing
+import shutil, os, multiprocessing, json
 
 gauth = GoogleAuth()
 drive = GoogleDrive(gauth)
 
 
 
-folder = '1Ss1nmv_wiTW-DHLtI8qUkQR-9YBk-FNP'
-
-
-def upload_file():
+def upload_files(folders):
     #this function is used to upload files to google drive
     #@except : print message if an error is found during file uploading
 
     try :
         for (root,dirs,files) in os.walk('record', topdown=True):
+            for i in folders :
+                #since our folder is like folder_name-file_name:
+                # Here we check if we've found a folder present in our folder name dictionary
+                ab = str((root.split("-")[0])).split("\\")
+                # print(folders[i], " -- ",ab[-1])
+                if folders[i] == ab[-1] :
+                    for file_name in files :
+                        #absolute path of the source file
+                        source = os.path.join(root, file_name)
 
-            if folder in root:
-                for file_name in files :
-                    #absolute path of the source file
-                    source = os.path.join(root, file_name)
-
-                    #Move the file to google drive
-                    # 1- Create the file into google drive location with the same name present on our machine
-                    file1 = drive.CreateFile({'parents': [{'id': folder}], 'title': file_name})
-                    #2- set the content of the file == to the content of the source file
-                    file1.SetContentFile(source)
-                    #upload the file
-                    file1.Upload()
-                    print('Moved:', file_name)
+                        #Move the file to google drive
+                        # 1- Create the file into google drive location with the same name present on our machine
+                        file1 = drive.CreateFile({'parents': [{'id': i}], 'title': file_name})
+                        #2- set the content of the file == to the content of the source file
+                        file1.SetContentFile(source)
+                        #upload the file
+                        file1.Upload()
+                        print('Moved:', file_name)
     except Exception as e:
         print("error ", e.message())
 
 
-def delete_folder_files():
+def delete_folder_files(folders):
     # this function is used to delete folder and files already uploaded
     # @except : print message if an error is found during file deletion
+    print('--------------------------------')
     try:
         for (root, dirs, files) in os.walk('record', topdown=True):
-            if folder in root:
-                # delete the file and the folder from where it belongs to
-                shutil.rmtree(root)
-                print('--------------------------------')
-                print('Deleted:', root)
+            for i in folders:
+                if folders[i] == str((root.split("-")[0])).split("\\")[-1]:
+                    # delete the file and the folder from where it belongs to
+                    shutil.rmtree(root)
+                    print('Deleted:', root)
 
     except Exception as e:
         print("error ", e.message())
 
 
 if __name__ == "__main__":
+
+    # Opening JSON file
+    file = open('config_folders.json')
+
+    # returns JSON object as
+    # a dictionary
+    folders = json.load(file)
+
+    # Closing file
+    file.close()
+
     #process to upload files
-    p1 = multiprocessing.Process(target=upload_file(), args=())
+    p1 = multiprocessing.Process(target=upload_files, args=(folders,))
     #process to delete folders related
-    p2 = multiprocessing.Process(target=delete_folder_files(), args=())
+    p2 = multiprocessing.Process(target=delete_folder_files, args=(folders,))
 
     # starting process 1
     p1.start()
@@ -65,5 +78,6 @@ if __name__ == "__main__":
     p2.join()
 
     # both processes finished
+    print('--------------------------------\n')
     print("Done!")
 
